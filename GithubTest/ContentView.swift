79df2b3f806455061704da782092a684c4fa4546
Justin Estes <jpestes37@gmail.com>
2021-09-24 22:06:19 -0500

    Initial Commit

---
+++ GithubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubTest
+//
+//  Created by Justin Estes on 9/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Justin Estes on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
